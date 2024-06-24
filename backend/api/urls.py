d202775b7cfb3cd500dfa9f288f09f50511654bf
Evan <me@evan.sh>
2024-06-24 09:37:22 +0000

    backend: re-organize, wip tournaments

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,24 +3,25 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
-from .views import (
-    UserProfileApiView, UserRegisterApiView, UserLoginApiView,
-    MeProfileApiView, UserMatchesApiView, MeRelationshipsApiView,
-    MeMatchesApiView, MeSettingsApiView, MeDeleteApiView,
-    MeHarvestApiView
-)
+
+from views.auth import *
+from views.users import *
+from views.tournaments import *
 
 urlpatterns = [
-    path('auth/register', UserRegisterApiView.as_view()),
+    path('auth/register', AuthRegister.as_view()),
     path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('users/@me/profile', MeProfileApiView.as_view()), # GET, PATCH
-    path('users/<userID>/profile', UserProfileApiView.as_view()), # GET
-    path('users/@me/matches', MeMatchesApiView.as_view()), # GET
-    path('users/<userID>/matches', UserMatchesApiView.as_view()), # GET
-    path('users/@me/settings', MeSettingsApiView.as_view()), # GET, PATCH
-    path('users/@me/relationships', MeRelationshipsApiView.as_view()), # GET, PUT
-    path('users/@me/harvest', MeHarvestApiView.as_view()), # POST, GET
-    path('users/@me/delete', MeDeleteApiView.as_view()), # POST
+    path('users/@me/profile', UserProfileMe.as_view()), # GET, PATCH
+    path('users/<userID>/profile', UserProfile.as_view()), # GET
+    path('users/@me/matches', UserMatchesMe.as_view()), # GET
+    path('users/<userID>/matches', UserMatches.as_view()), # GET
+    path('users/@me/settings', UserSettingsMe.as_view()), # GET, PATCH
+    path('users/@me/relationships', UserRelationshipsMe.as_view()), # GET, PUT
+    path('users/@me/harvest', UserHarvestMe.as_view()), # POST, GET, DELETE
+    path('users/@me/delete', UserDeleteMe.as_view()), # POST, GET, DELETE
+
+    # path('tournaments/<tournamentID>', TournamentInfo.as_view()), # GET, DELETE, PATCH
+    # path('tournaments', TournamentCreate.as_view()), # POST
 ]
---
+++ backend/api/views/auth.py
@@ -0,0 +1,69 @@
+import re
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password
+
+from ..models import User
+from ..serializers import UserSerializer
+from ..util import generate_id, generate_jwt
+
+@permission_classes([AllowAny])
+class AuthRegister(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = self.validate_request_data(request.data)
+        except ValidationError as ex:
+            return Response({"error": str(ex)}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=data['username']).exists():
+            return Response({"error": "Username is already taken"}, status=status.HTTP_409_CONFLICT)
+        
+        if User.objects.filter(email=data['email']).exists():
+            return Response({"error": "Email is already in use"}, status=status.HTTP_409_CONFLICT)
+
+        user = User.objects.create(
+            userID=generate_id('user'),
+            username=data['username'],
+            displayName=data['username'],
+            email=data['email'],
+            avatarID='default',
+            password=make_password(data['password']),
+            lang=data['lang']
+        )
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def validate_request_data(self, data):
+        required_fields = {'username', 'email', 'password', 'lang'}
+        if not all(field in data for field in required_fields):
+            raise ValidationError("All fields ['username', 'email', 'password', 'lang'] are required.")
+
+        username, email, password, lang = data['username'], data['email'], data['password'], data['lang']
+
+        if len(username) < 4:
+            raise ValidationError("Username must be at least 4 characters long.")
+        if len(username) > 16:
+            raise ValidationError("Username cannot be longer than 16 characters.")
+        if len(email) > 64:
+            raise ValidationError("Email cannot be longer than 64 characters.")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValidationError("Invalid email address, did not match regex pattern.")
+        if len(password) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        if len(password) > 128:
+            raise ValidationError("Password cannot be longer than 128 characters.")
+        if len(lang) != 2 or lang not in ['en', 'fr']:
+            raise ValidationError("Unsupported language. Supported languages are 'en' and 'fr'.")
+
+        return {
+            "username": username,
+            "email": email,
+            "password": password,
+            "lang": lang
+        }
