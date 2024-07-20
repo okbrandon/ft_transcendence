06e4abd15d6af5e439074c53521bd489183d795e
Hanmin <hanmin.01.park@gmail.com>
2024-07-20 17:13:38 +0200

    ✨ Animation: Added a little bit of animation using framer-motion

--- frontend/src/components/features/Authentication/Authentication.js
+++ frontend/src/components/features/Authentication/Authentication.js
@@ -5,6 +5,7 @@ import Stack from 'react-bootstrap/Stack';
 import { PongBall, PongBar } from '../../styles/animations/PongBackground.styled';
 import Title from '../../styles/shared/Title.styled';
 import Container from '../../styles/layouts/Container.styled';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Authentication = () => {
 	const navigate = useNavigate();
@@ -17,17 +18,22 @@ const Authentication = () => {
 	};
 
 	return (
-		<>
-			<PongBar/>
-			<PongBall/>
-			<Container>
-				<Title>PONG</Title>
-				<Stack gap={5} className="mx-auto">
-					<PongButton title="Login" variant="light" onClick={handleLogin}/>
-					<PongButton title="Sign Up" variant="light" onClick={handleSignUp}/>
-				</Stack>
-			</Container>
-		</>
+		<AnimatePresence>
+			<motion.div
+				initial={{opacity: 0}}
+				animate={{opacity: 1}}
+				transition={{duration: 1.5}}>
+				<PongBar/>
+				<PongBall/>
+				<Container>
+					<Title>PONG</Title>
+					<Stack gap={5} className="mx-auto">
+						<PongButton title="Login" variant="light" onClick={handleLogin}/>
+						<PongButton title="Sign Up" variant="light" onClick={handleSignUp}/>
+					</Stack>
+				</Container>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
--- frontend/src/components/shared/PongButton/index.js
+++ frontend/src/components/shared/PongButton/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import StyledPongButton from '../../styles/shared/button/PongButton.styled';
+import { motion } from 'framer-motion';
 
 const PongButton = ({ title, variant, onClick }) => {
 	return (
-		<StyledPongButton variant={variant} onClick={onClick}>
-			{title || 'Pong'}
-		</StyledPongButton>
+		<motion.div
+			whileHover={{ scale: [null, 1.10, 1.08] }}
+			transition={{ duration: 0.3 }}>
+			<StyledPongButton variant={variant} onClick={onClick}>
+				{title || 'Pong'}
+			</StyledPongButton>
+		</motion.div>
 	);
 };
 
