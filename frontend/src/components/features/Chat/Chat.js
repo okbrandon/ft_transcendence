fae26e7cd0d3b7b37b469c7aef3760543e827c28
Kariyu42 <rioukian01@gmail.com>
2024-07-26 16:30:30 +0800

    💬: debut chat

---
+++ frontend/src/components/features/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Chat = () => {
+
+	return (
+		<header className='msg-window-header'>
+			<div className='msg-window-container'></div>
+		</header>
+	);
+};
+
+export default Chat;
