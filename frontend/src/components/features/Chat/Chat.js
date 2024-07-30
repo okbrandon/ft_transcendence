117a7cab54f59e521d663b6cb9b95460f667fe23
Kariyu42 <rioukian01@gmail.com>
2024-07-30 15:16:06 +0800

    💬: added variables for storing messages + functions

--- frontend/src/components/features/Chat/Chat.js
+++ frontend/src/components/features/Chat/Chat.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 
 const Chat = () => {
 
+	const [message, setMessage] = useState('[]'); // message variable used to store the messages
+	const [input, setInput] = useState(''); // input variable used to store the input value
+
+	const handleSend = () => {
+		// check if input is empty
+		if (input === '') {
+			return;
+		}
+		setMessage([...message, input]); // add the input value to the message
+		setInput(''); // clear the input value
+	}
+
 	return (
 		<header className='msg-window-header'>
-			<div className='msg-window-container'></div>
+			<div className='msg-window-container'>
+				<div className='msg-window-details'>
+					<div className='msg-avatar'>
+						{/* img avatar profile */}
+					</div>
+					<button className='msg-username'>
+						<span>Messages</span>
+					</button>
+				</div>
+			</div>
 		</header>
 	);
 };
