f4b5fe6408bb3ec899fa337568429527b1c0e7a1
hanmpark <hanmin.01.park@gmail.com>
2024-08-05 13:04:57 +0200

    Match History: Match Card components slide down animation

--- frontend/src/components/features/Profile/MatchHistory.js
+++ frontend/src/components/features/Profile/MatchHistory.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MatchCardContainer from "../../styles/layouts/profile/MatchCardContainer.styled";
 import MatchCard from "../../styles/shared/profile/matchCard.styled";
+import { AnimatePresence } from "framer-motion";
 
 const matchArrayTest = [
 	{player1: "roger", player2: "bradon", winner: "bradon", score1: 20, score2: 21, won: true},
@@ -13,30 +14,53 @@ const matchArrayTest = [
 	{player1: "ian", player2: "bradon", winner: "ian", score1: 20, score2: 19, won: false},
 ];
 
+const variants = {
+	hidden: { opacity: 0, y: -10 },
+	visible: (i) => ({
+		opacity: 1,
+		y: 0,
+		transition: {
+		delay: i * 0.1,
+		duration: 0.5,
+		},
+	}),
+};
+
 const MatchHistory = () => {
 	return (
 		<div>
 			{
-				matchArrayTest.length === 0 ? <p>No matches played yet</p> :
-				matchArrayTest.map((match, index) => (
-						<MatchCardContainer key={index} $won={match.won}>
-							<MatchCard>
-								<tbody>
-									<tr>
-										<td>
-											<h3>{match.player1} vs {match.player2}</h3>
-										</td>
-										<td style={{textAlign: 'center'}}>
-											<h3><strong>{match.won ? 'Victory' : 'Defeat'}</strong></h3>
-										</td>
-										<td>
-											<h1>{match.score1} - {match.score2}</h1>
-										</td>
-									</tr>
-								</tbody>
-							</MatchCard>
-						</MatchCardContainer>
-					)
+				matchArrayTest.length === 0 ? (
+					<p>No matches played yet</p>
+				) : (
+					<AnimatePresence>
+						{matchArrayTest.map((match, index) => (
+								<MatchCardContainer
+									key={index}
+									$won={match.won}
+									initial="hidden"
+									animate="visible"
+									custom={index}
+									variants={variants}
+								>
+									<MatchCard>
+										<tbody>
+											<tr>
+												<td>
+													<h3>{match.player1} vs {match.player2}</h3>
+												</td>
+												<td style={{textAlign: 'center'}}>
+													<h3 style={{fontWeight: 'bold'}}>{match.won ? 'Victory' : 'Defeat'}</h3>
+												</td>
+												<td>
+													<h1>{match.score1} - {match.score2}</h1>
+												</td>
+											</tr>
+										</tbody>
+									</MatchCard>
+								</MatchCardContainer>
+						))}
+					</AnimatePresence>
 				)
 			}
 		</div>
--- frontend/src/components/styles/layouts/profile/MatchCardContainer.styled.js
+++ frontend/src/components/styles/layouts/profile/MatchCardContainer.styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const MatchCardContainer = styled.div`
-	padding: 20px;
+const MatchCardContainer = styled(motion.div)`
+	padding: 10px;
 	margin: 10px;
 	border-radius: 20px;
 	background: ${({ $won }) => $won ? 'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(36,210,0,1) 100%)' : 'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(255,0,0,1) 100%)'};
