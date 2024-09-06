24001c1729ac22de3545fc8731cc531ac45bc737
Kariyu42 <rioukian01@gmail.com>
2024-09-06 17:07:27 +0800

    🏫Leaderboard: Table added

--- frontend/src/components/Leaderboard/Leaderboard.js
+++ frontend/src/components/Leaderboard/Leaderboard.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavContainer } from '../../styles/Home/Navigation.styled';
 
 const WrapContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
 	margin: 70px 50px 30px 50px;
 `;
 
@@ -15,10 +14,10 @@ const WrapContainer = styled.div`
 const TopDetails = styled.div`
 	display: flex;
 	flex-direction: row;
-	justify-content: center;
+	justify-content: space-around;
 `;
 
-const LowerRankingContainer = styled.div`
+const LeftChildContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 `;
@@ -27,18 +26,15 @@ const RankingHeader = ({ rank }) => {
 	return <h3>Your Rank: {rank}</h3>;
 };
 
-const LowerLeftStatsContainer = styled.div`
+const ScorePosContainer = styled.div`
 	display: flex;
 	flex-direction: row;
-`;
-
-const LowerRightContainer = styled.div`
-	display: flex;
-	flex-direction: row;
+	justify-content: flex-start;
 `;
 
 const StatItem = styled.div`
 	margin-right: 10px;
+	opacity: 0.8;
 `;
 
 const ScoreDisplay = ({ score }) => {
@@ -49,12 +45,35 @@ const PositionDisplay = ({ position }) => {
 	return <StatItem>Your Position: {position}</StatItem>;
 };
 
-const NavBarContainer = styled.div`
+const RightChildContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+const TopButtonDropDown = styled.div`
+	display: flex;
+	justify-content: center;
+	margin-bottom: 5px;
+`;
+
+const Nav = () => {
+	return (
+		<TopButtonDropDown>
+			<NavDropdown title="Top Win" id="nav-dropdown" menuVariant='dark'>
+				<NavDropdown.Item eventKey="4.1">Top Win</NavDropdown.Item>
+				<NavDropdown.Item eventKey="4.2">Top Loss</NavDropdown.Item>
+				<NavDropdown.Item eventKey="4.3">Top Draw</NavDropdown.Item>
+				<NavDropdown.Item eventKey="4.4">Top Win Rate</NavDropdown.Item>
+			</NavDropdown>
+		</TopButtonDropDown>
+	);
+};
+
+const TimeButtonContainer = styled.div`
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
-	margin-left: 10px;
-	
 `;
 
 const ButtonWrapper = styled.div`
@@ -62,34 +81,43 @@ const ButtonWrapper = styled.div`
 	flex-direction: row;
 	justify-content: center;
 	margin-right: 7px;
-	margin-top: 50%;
 
 	&:last-child {
 		margin-right: 0;
 	}
 `;
 
+const TableLeaderboard = styled.table`
+	border-collapse: collapse;
+	width: 100%;
+	margin-top: 20px;
+
+	th, td {
+		border-top: 1px solid #ddd;
+		padding: 8px;
+	}
+	
+	th {
+		background-color: grey;
+		color: white;
+		text-align: center;
+	}
+`;
+
 export const Leaderboard = () => {
 	return (
-		// Parent container
 		<WrapContainer>
 			<TopDetails>
-				<LowerRankingContainer>
+				<LeftChildContainer>
 					<RankingHeader rank="1st"/>
-					<LowerLeftStatsContainer>
-						<ScoreDisplay score="29"/>
+					<ScorePosContainer>
+						<ScoreDisplay score="18975"/>
 						<PositionDisplay position="1st"/>
-					</LowerLeftStatsContainer>
-				</LowerRankingContainer>
-				<NavBarContainer>
-					<NavDropdown title="Top Win" id="nav-dropdown" menuVariant='dark'>
-						<NavDropdown.Item eventKey="4.1">Top Win</NavDropdown.Item>
-						<NavDropdown.Item eventKey="4.2">Top Loss</NavDropdown.Item>
-						<NavDropdown.Item eventKey="4.3">Top Draw</NavDropdown.Item>
-						<NavDropdown.Item eventKey="4.4">Top Win Rate</NavDropdown.Item>
-					</NavDropdown>
-				</NavBarContainer>
-				<LowerRightContainer>
+					</ScorePosContainer>
+				</LeftChildContainer>
+				<RightChildContainer>
+					<Nav/>
+					<TimeButtonContainer>
 					<ButtonWrapper>
 						<Button variant="secondary" size="sm">Daily</Button>
 					</ButtonWrapper>
@@ -99,43 +127,35 @@ export const Leaderboard = () => {
 					<ButtonWrapper>
 						<Button variant="secondary" size="sm">Lifetime</Button>
 					</ButtonWrapper>
-				</LowerRightContainer>
+					</TimeButtonContainer>
+				</RightChildContainer>
 			</TopDetails>
+			<TableLeaderboard>
+				<thead>
+					<tr>
+						<th>Pos</th>
+						<th>Player Name</th>
+						<th>Score</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Alice</td>
+						<td>100</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Brandon</td>
+						<td>90</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Hanmin</td>
+						<td>80</td>
+					</tr>
+				</tbody>
+			</TableLeaderboard>
 		</WrapContainer>
-				// Child Ranking Container
-					// Player Rankings <div>
-					// Your score <div>
-					// Position <div>
-				// Child TOP WIN/LOSS Container
-					// Daily <button>
-					// Weekly <button>
-					// Lifetime <button>
-			// Lower Child Container
-				// <table>
-					// <thead>
-						// <tr>
-							// <th>Pos</th>
-							// <th>Player Name</th>
-							// <th>Score</th>
-						// </tr>
-					// </thead>
-					// Add content examples in <tbody>
-						// <tr>
-							// <td>1</td>
-							// <td>Alice</td>
-							// <td>100</td>
-						// </tr>
-						// <tr>
-							// <td>2</td>
-							// <td>Brandonation</td>
-							// <td>90</td>
-						// </tr>
-						// <tr>
-							// <td>3</td>
-							// <td>Hanministrateur</td>
-							// <td>80</td>
-						// </tr>
-					// </tbody>
-				// </table>
 	);
 };
