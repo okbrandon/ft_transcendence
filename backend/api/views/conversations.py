f08b3164854d0ccb6c1fdda3ba0720d8b68cbc32
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-09-14 11:05:24 +0200

    chat: removed sensitive data

--- backend/api/views/conversations.py
+++ backend/api/views/conversations.py
@@ -6,10 +6,18 @@ from rest_framework.views import APIView
 
 from ..models import Conversation
 from ..serializers import ConversationSerializer
+from ..util import get_safe_profile
 
 class ConversationListView(APIView):
     def get(self, request, *args, **kwargs):
         me = request.user
         conversations = me.conversations.all()
         serializer = ConversationSerializer(conversations, many=True)
-        return Response({'conversations': serializer.data}, status=status.HTTP_200_OK)
+        
+        safe_conversations = serializer.data
+        
+        for conversation in safe_conversations:
+            participants = conversation.get('participants', [])
+            conversation['participants'] = get_safe_profile(participants, me=False, many=True)
+
+        return Response({'conversations': safe_conversations}, status=status.HTTP_200_OK)
