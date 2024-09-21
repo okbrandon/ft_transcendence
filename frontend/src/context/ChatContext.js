93068e90eb420b6856c22553ecc8fc855bc510f9
hanmpark <hanmin.01.park@gmail.com>
2024-09-21 18:42:15 +0200

    Merge: Added port number :8000 consumers.py

--- frontend/src/context/ChatContext.js
+++ frontend/src/context/ChatContext.js
@@ -10,141 +10,141 @@ export const ChatContext = createContext({
 
 const ChatProvider = ({ children }) => {
 	const [conversations, setConversations] = useState([
-        {
-          "conversationID": "conv_MTcyNjMwMjg5NjQ2MDc3Mg",
-          "conversationType": "private_message",
-          "participants": [
-            {
-              "userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
-              "username": "bsoubaig",
-              "displayName": null,
-              "lang": "fr",
-              "avatarID": null,
-              "bannerID": null,
-              "bio": null,
-              "flags": 1
-            },
-            {
-              "userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
-              "username": "evmorvan",
-              "displayName": null,
-              "lang": "fr",
-              "avatarID": null,
-              "bannerID": null,
-              "bio": null,
-              "flags": 1
-            }
-          ],
-          "messages": [
-            {
-              "messageID": "msg_MTcyNjMxOTU5OTk4MDg4MzI",
-              "content": "Hello, how are you?",
-              "sender": {
-                "userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
-                "username": "bsoubaig",
-                "displayName": null,
-                "lang": "fr",
-                "avatarID": null,
-                "bannerID": null,
-                "bio": null,
-                "flags": 1
-              },
-              "createdAt": "2024-09-14T13:15:22.116212Z"
-            },
-            {
-              "messageID": "msg_MTcyNjMxOTk2NDg4MjY2Mjc",
-              "content": "YOU STINK BRO LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-              "sender": {
-                "userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
-                "username": "bsoubaig",
-                "displayName": null,
-                "lang": "fr",
-                "avatarID": null,
-                "bannerID": null,
-                "bio": null,
-                "flags": 1
-              },
-              "createdAt": "2024-09-14T13:20:05.103983Z"
-            }
-          ]
-        },
-        {
-          "conversationID": "conv_MTcyNjMwMjg5NjQ2MDc7Mg",
-          "conversationType": "private_message",
-          "participants": [
-            {
-              "userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
-              "username": "johndoe",
-              "displayName": null,
-              "lang": "en",
-              "avatarID": null,
-              "bannerID": null,
-              "bio": null,
-              "flags": 1
-            },
-            {
-              "userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
-              "username": "janedoe",
-              "displayName": null,
-              "lang": "en",
-              "avatarID": null,
-              "bannerID": null,
-              "bio": null,
-              "flags": 1
-            }
-          ],
-          "messages": [
-            {
-              "messageID": "msg_MTcyNjMxOTU5OTk4MDg4MzI",
-              "content": "Hi, nice to meet you!",
-              "sender": {
-                "userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
-                "username": "johndoe",
-                "displayName": null,
-                "lang": "en",
-                "avatarID": null,
-                "bannerID": null,
-                "bio": null,
-                "flags": 1
-              },
-              "createdAt": "2024-09-14T13:15:22.116212Z"
-            },
-            {
-              "messageID": "msg_MTcyNjMxOTk2NDg4MjY2Mjc",
-              "content": "Nice to meet you too!",
-              "sender": {
-                "userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
-                "username": "janedoe",
-                "displayName": null,
-                "lang": "en",
-                "avatarID": null,
-                "bannerID": null,
-                "bio": null,
-                "flags": 1
-              },
-              "createdAt": "2024-09-14T13:20:05.103983Z"
-            }
-          ]
-        }
-      ]);
+				{
+					"conversationID": "conv_MTcyNjMwMjg5NjQ2MDc3Mg",
+					"conversationType": "private_message",
+					"participants": [
+						{
+							"userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
+							"username": "bsoubaig",
+							"displayName": null,
+							"lang": "fr",
+							"avatarID": null,
+							"bannerID": null,
+							"bio": null,
+							"flags": 1
+						},
+						{
+							"userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
+							"username": "evmorvan",
+							"displayName": null,
+							"lang": "fr",
+							"avatarID": null,
+							"bannerID": null,
+							"bio": null,
+							"flags": 1
+						}
+					],
+					"messages": [
+						{
+							"messageID": "msg_MTcyNjMxOTU5OTk4MDg4MzI",
+							"content": "Hello, how are you?",
+							"sender": {
+								"userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
+								"username": "bsoubaig",
+								"displayName": null,
+								"lang": "fr",
+								"avatarID": null,
+								"bannerID": null,
+								"bio": null,
+								"flags": 1
+							},
+							"createdAt": "2024-09-14T13:15:22.116212Z"
+						},
+						{
+							"messageID": "msg_MTcyNjMxOTk2NDg4MjY2Mjc",
+							"content": "YOU STINK BRO LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+							"sender": {
+								"userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
+								"username": "bsoubaig",
+								"displayName": null,
+								"lang": "fr",
+								"avatarID": null,
+								"bannerID": null,
+								"bio": null,
+								"flags": 1
+							},
+							"createdAt": "2024-09-14T13:20:05.103983Z"
+						}
+					]
+				},
+				{
+					"conversationID": "conv_MTcyNjMwMjg5NjQ2MDc7Mg",
+					"conversationType": "private_message",
+					"participants": [
+						{
+							"userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
+							"username": "johndoe",
+							"displayName": null,
+							"lang": "en",
+							"avatarID": null,
+							"bannerID": null,
+							"bio": null,
+							"flags": 1
+						},
+						{
+							"userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
+							"username": "janedoe",
+							"displayName": null,
+							"lang": "en",
+							"avatarID": null,
+							"bannerID": null,
+							"bio": null,
+							"flags": 1
+						}
+					],
+					"messages": [
+						{
+							"messageID": "msg_MTcyNjMxOTU5OTk4MDg4MzI",
+							"content": "Hi, nice to meet you!",
+							"sender": {
+								"userID": "user_MTcyNjMwMTc5NTA5MjQ1MjA",
+								"username": "johndoe",
+								"displayName": null,
+								"lang": "en",
+								"avatarID": null,
+								"bannerID": null,
+								"bio": null,
+								"flags": 1
+							},
+							"createdAt": "2024-09-14T13:15:22.116212Z"
+						},
+						{
+							"messageID": "msg_MTcyNjMxOTk2NDg4MjY2Mjc",
+							"content": "Nice to meet you too!",
+							"sender": {
+								"userID": "user_MTcyNjMwMTkxMzk0Nzg5NTU",
+								"username": "janedoe",
+								"displayName": null,
+								"lang": "en",
+								"avatarID": null,
+								"bannerID": null,
+								"bio": null,
+								"flags": 1
+							},
+							"createdAt": "2024-09-14T13:20:05.103983Z"
+						}
+					]
+				}
+			]);
 
-	const { sendMessage } = useWebSocket(WS_URL + localStorage.getItem('token'), {
-		onOpen: () => console.log(`WebSocket connection opened:`),
-		onMessage: (event) => {
-			const data = JSON.parse(event.data);
-			console.log(data);
-			if (data.type === 'conversation_update') {
-				API.get('chat/conversations')
-					.then((response) => {
-						console.log('(ChatMessages); Harvested data: ', response.data.conversations);
-						setConversations(response.data.conversations);
-					})
-					.catch((error) => {
-						console.log('FAILED TO HARVEST: ', error);
-					});
-			}
-		},
-	});
+useWebSocket(WS_URL + localStorage.getItem('token'), {
+	onOpen: () => console.log(`WebSocket connection opened:`),
+	onMessage: (event) => {
+		const data = JSON.parse(event.data);
+		console.log(data);
+		if (data.type === 'conversation_update') {
+			API.get('chat/conversations')
+				.then((response) => {
+					console.log('(ChatMessages); Harvested data: ', response.data.conversations);
+					setConversations(response.data.conversations);
+				})
+				.catch((error) => {
+					console.log('FAILED TO HARVEST: ', error);
+				});
+		}
+	},
+});
 
 	return (
 		<ChatContext.Provider value={{ conversations }}>
