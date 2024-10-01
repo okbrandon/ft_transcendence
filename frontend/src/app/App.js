f6b3df7a9dcabdf660d119ada062f7018ec77d5d
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-10-01 11:31:08 +0200

    frontend: Implemented i18n to base project

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -4,14 +4,21 @@ import Router from './Router';
 import { RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Suspense } from 'react';
 
-const App = () => {
+function App() {
   return (
     <>
       <GlobalStyles/>
       <RouterProvider router={Router}/>
     </>
   );
-};
+}
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="Loading...">
+      <App/>
+    </Suspense>
+  );
+}
