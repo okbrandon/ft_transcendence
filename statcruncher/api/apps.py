bc5a35a7fb800cd6321ec9610704e67beeb0b722
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-10-03 10:41:35 +0200

    statcruncher: Running StatCruncher in production

--- statcruncher/api/apps.py
+++ statcruncher/api/apps.py
@@ -1,5 +1,33 @@
+import sys
+import logging
+
 from django.apps import AppConfig
+
+from .backends.database import StatDatabase
+from .backends.statcruncher import StatCruncher
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+DATABASE = None
+STATCRUNCHER = None
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        is_manage = 'manage.py' in sys.argv
+        is_dev_mode = 'runserver' in sys.argv or '--in-development' in sys.argv
+
+        if is_manage or is_dev_mode:
+            logger.debug("StatCruncher is disabled in development mode.")
+            return
+
+        logger.debug("Starting the StatCruncher...")
+
+        global DATABASE
+        global STATCRUNCHER
+
+        DATABASE = StatDatabase()
+        STATCRUNCHER = StatCruncher(DATABASE)
--- statcruncher/start.sh
+++ statcruncher/start.sh
@@ -9,4 +9,4 @@ done
 python3 manage.py migrate
 
 # Start the server
-python3 manage.py runserver_plus --cert-file /certs/server.crt --key-file /certs/server.key 0.0.0.0:9000
+gunicorn --certfile=/certs/server.crt --keyfile=/certs/server.key --bind 0.0.0.0:9000 statcruncher.wsgi:application
