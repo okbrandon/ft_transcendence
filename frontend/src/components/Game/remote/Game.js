ed970e7c9f4d03181a9c9706c581ee7f77521fc1
Hanmin Park <hanmin.01.park@gmail.com>
2024-10-21 19:34:33 +0200

    Game: Added animation on each hit

--- frontend/src/components/Game/remote/Game.js
+++ frontend/src/components/Game/remote/Game.js
@@ -133,6 +133,9 @@ const Game = () => {
 				case 'PADDLE_HIT':
 					setHitPos(data.d.ball);
 					break;
+				case 'BALL_HIT':
+					setHitPos(data.d.ball);
+					break;
 				default:
 					console.log('Unhandled message:', data);
 			}
