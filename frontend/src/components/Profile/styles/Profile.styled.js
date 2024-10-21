4492b30cae9f5aaf67baa8548bbd4da22a4078fd
Hanmin Park <hanmin.01.park@gmail.com>
2024-10-21 20:10:21 +0200

    Profile: Added Loader when switching profile

--- frontend/src/components/Profile/styles/Profile.styled.js
+++ frontend/src/components/Profile/styles/Profile.styled.js
@@ -168,6 +168,12 @@ export const MatchCardTable = styled.table`
 		border-bottom: 1px solid #2e2e3d;
 	}
 
+	td.hover {
+		&:hover {
+			color: rgba(164, 69, 178, 1);
+		}
+	}
+
 	tbody {
 		display: block;
 		max-height: 600px;
