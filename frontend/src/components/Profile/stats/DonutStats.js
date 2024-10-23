083391bb30f1a2acdae2d4d17caebe7446c4aeef
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-10-23 11:58:32 +0200

    Profile: Fixed donut chart tooltips win/loss

--- frontend/src/components/Profile/stats/DonutStats.js
+++ frontend/src/components/Profile/stats/DonutStats.js
@@ -10,8 +10,21 @@ const DonutStats = ({ series, percentage }) => {
 		labels: [t('profile.stats.donut.labels.first'), t('profile.stats.donut.labels.second')],
 		stroke: { width: 0 },
 		legend: { show: false },
+		fillSeriesColor: true,
 		fill: { colors: ['#8BD742', '#F74D52'] },
-		tooltip: { enabled: true },
+		tooltip: {
+			enabled: true,
+			custom: function({ seriesIndex, w }) {
+				const tooltipColor = w.config.fill.colors[seriesIndex];
+				const label = w.globals.labels[seriesIndex];
+				const value = w.globals.series[seriesIndex];
+
+				return `
+					<div style="background-color: ${tooltipColor}; padding: 10px; border-radius: 1px; color: white;">
+						<strong>${label}</strong>: ${value}
+					</div>`;
+			}
+		},
 		plotOptions: {
 			pie: {
 				expandOnClick: false,
