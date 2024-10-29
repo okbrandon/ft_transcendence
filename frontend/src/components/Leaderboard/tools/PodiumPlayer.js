470b58dcda39f15a90da1c2560ca42fc90a39600
Brandon <103316367+okbrandon@users.noreply.github.com>
2024-10-29 11:31:15 +0100

    Leaderboard: Fixed Podium, now using displayName

--- frontend/src/components/Leaderboard/tools/PodiumPlayer.js
+++ frontend/src/components/Leaderboard/tools/PodiumPlayer.js
@@ -10,11 +10,12 @@ import {
 	PodiumBase,
 	Badge,
 } from '../styles/Podium.styled';
+import { formatUserData } from '../../../api/user';
 
 const PodiumPlayer = ({ player, position, selectedStat }) => {
 	const navigate = useNavigate();
 	const positionClass = position === 0 ? 'second' : position === 1 ? 'first' : 'third';
-	const playerName = player?.user.username || 'N/A';
+	const formattedPlayer = player ? formatUserData(player.user) : { displayName: 'N/A' };
 	const playerScore = player?.stats[selectedStat] || 0;
 
 	const handleClickUsername = (username) => {
@@ -23,11 +24,11 @@ const PodiumPlayer = ({ player, position, selectedStat }) => {
 
 	return (
 		<PodiumBase className={positionClass}>
-			<PlayerAvatar src={player?.user.avatarID ? player?.user.avatarID : 'images/default-profile.png'} alt={playerName} $position={positionClass} />
+			<PlayerAvatar src={formattedPlayer.avatarID ? formattedPlayer.avatarID : 'images/default-profile.png'} alt={formattedPlayer.displayName} $position={positionClass} />
 			<PlayerInfo $position={positionClass}>
 				<div style={{paddingTop: '10px', fontWeight: 'bold'}}>
-					{playerName !== 'N/A' ? (
-						<Username onClick={() => handleClickUsername(playerName)}>{playerName}</Username>
+					{formattedPlayer.displayName !== 'N/A' ? (
+						<Username onClick={() => handleClickUsername(formattedPlayer.username)}>{formattedPlayer.displayName}</Username>
 					) : (
 						<span style={{color: 'rgba(255, 255, 255, 0.5)', cursor: 'not-allowed'}}>N/A</span>
 					)}
