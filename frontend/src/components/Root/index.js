f0d6c84966163fd67048c57b6dc94127911d51b4
Hanmin <hanmin.01.park@gmail.com>
2024-11-05 23:38:15 +0100

    Music: Add background music

--- frontend/src/components/Root/index.js
+++ frontend/src/components/Root/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../Navigation/Navigation';
 import Chat from '../Chat/Chat';
@@ -12,14 +12,35 @@ const Root = () => {
 	const location = useLocation();
 	const { isLoggedIn } = useAuth();
 	const [showPersistentUI, setShowPersistentUI] = useState(true);
+	const [hasInteracted, setHasInteracted] = useState(false);
+	const [audio] = useState(new Audio('/sounds/pong-theme.mp3'));
+
+	const activateMusic = useCallback(() => {
+		if (!hasInteracted) {
+			setHasInteracted(true);
+		}
+	}, [hasInteracted]);
 
 	useEffect(() => {
-		if (location.pathname.includes('/game')) {
-			setShowPersistentUI(false);
-		} else {
-			setShowPersistentUI(true);
+		setShowPersistentUI(!location.pathname.includes('/game'));
+
+		audio.loop = true;
+		audio.volume = 0.1;
+
+		document.body.addEventListener('click', activateMusic);
+
+		return () => {
+			document.body.removeEventListener('click', activateMusic);
 		}
-	}, [location]);
+	}, [location, audio, activateMusic]);
+
+	useEffect(() => {
+		if (hasInteracted && showPersistentUI && isLoggedIn) {
+			audio.play();
+		} else if (!showPersistentUI) {
+			audio.pause();
+		}
+	}, [hasInteracted, showPersistentUI, audio, isLoggedIn]);
 
 	return (
 		<>
