0a46887b0d1dd12e55f316a82901559e247ac742
hanmpark <hanmin.01.park@gmail.com>
2024-11-11 17:48:53 +0100

    Tournament: Fix styling for create tournament page

--- frontend/src/components/Game/styles/Tournament/CreateTournament.styled.js
+++ frontend/src/components/Game/styles/Tournament/CreateTournament.styled.js
@@ -4,8 +4,7 @@ export const PageContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
-	padding: 2rem;
+	padding-top: calc(80px + 2rem);
 	color: #fff;
 	height: 100vh;
 	width: 100%;
